5fabceae78ed6e9e78619e4f7b735f6fc4bacd86
Kunj Ramoliya <kunjramoliya367@gmail.com>
2025-07-17 22:53:34 +0530

    2

---
+++ Java/two.java
@@ -0,0 +1,11 @@
+// 2 WAP to find whether string is palindrome or not. 
+// D-3
+import java.util.*;
+
+public class two {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        sc.close();
+    }
+}
