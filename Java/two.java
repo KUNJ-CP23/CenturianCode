9e03ba730d39e780c67942ddbc586ea4ecdc8a3a
Kunj Ramoliya <kunjramoliya367@gmail.com>
2025-08-27 22:59:34 +0530

    2java

--- Java/two.java
+++ Java/two.java
@@ -1,15 +1,16 @@
 // 2 WAP to find whether string is palindrome or not. 
 // D-3
+
 import java.util.*;
 
-public class two {
-    public static void main(String[] args) {
+public class two{
+    public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string: ");
         String str = sc.nextLine();
 
         String reversed = "";
-        for(int i=str.length()-1 ;i>=0;i--){
+        for(int i=str.length()-1 ; i>=0 ; i--){
             reversed += str.charAt(i);
         }
 
